be4938772c2f8cfae52368b98f253e2c7b216db5
DAnii <DAnii@no.com>
2017-11-29 17:54:25 +0100

    python version suchen

--- PyNG.py
+++ PyNG.py
@@ -4,16 +4,26 @@ import threading
 import sys
 from subprocess import call
 
+
 def start_client():
-    call(["python", "chat_client.py" , "localhost", "1337"])
+    call(["python2.7", "chat_client.py" , "localhost", "9009"])
 
 def start_server():
-    call(["python", "chat_server.py"])
+    call(["python2.7", "chat_server.py"])
+
+def find_python():
+	#assert sys.version_info>= (2,7)
+	#print( "your python version is") , sys.version_info
+
+
+	print("tut noch net")
+
 
 def main():
+   find_python()
    parser = argparse.ArgumentParser(description=
             'Network games Server/Client',version="%prog 1.0")
-  
+
    parser.add_argument("-l", "--list",
       action='store_true',
       #default="False",
@@ -30,26 +40,26 @@ def main():
    args = parser.parse_args()
 
    if args.list != False:
-      print "List all the servers"
+      print( "List all the servers")
    elif args.connect:
-      print "connect to ", args.connect
+      print( "connect to "), args.connect
       #os.system("chat_server.py 1")
       processThread = threading.Thread(target=start_server)  # <- note extra ','
       processThread.start()
    elif args.start:
-      print "Start server"
+      print( "Start server")
       os.system("chat_client.py 1")
    else:
-      print "wooops"
+      print( "wooops")
 
-   print args
+   print( args)
 
    text = raw_input("Join server ?")  # Python 2
    if text == "yes":
       processThread = threading.Thread(target=start_client)  # <- note extra ','
       processThread.start()
    else:
-      print "wooops"
-      
+      print("wooops")
+
 if __name__ == "__main__":
    main()
